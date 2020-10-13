84056f0741e648aa692269e49df98ef798e7add1
callum vass <callum.vass@gmail.com>
2020-10-13 19:53:53 +0100

    Initial

---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿open Giraffe
+open Saturn
+open System.Security.Claims
+
+let loggedIn = pipeline {
+  requires_authentication (Giraffe.Auth.challenge "Google")
+}
+
+let top = router {
+  pipe_through loggedIn
+  get "/" (fun next ctx ->
+      (ctx.User.Identity.Name |> json) next ctx)
+}
+
+// Find these information in the App Registration part of you Azure Active Directory (http://aka.ms/AppRegistrations).
+let clientId = "<ClientId>"
+let clientSecret = "<ClientSecret>"
+let callbackPath = "/signin-google"
+
+let jsonMappings =
+    [ "id", ClaimTypes.NameIdentifier
+      "displayName", ClaimTypes.Name ]
+
+let app = application {
+  use_router top
+  url "http://[::]:8085/"
+  memory_cache
+  use_gzip
+  use_google_oauth clientId clientSecret callbackPath jsonMappings
+}
+
+[<EntryPoint>]
+let main _ =
+  run app
+  0 // return an integer exit code
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Saturn OAuth with Google
+
+To run replace the `clientId` and `clientSecret` variables in `Program.fs` with your app's secret which you can create at: https://console.developers.google.com/
